5496d9ea424616a470bfb2fa97fe73257dd85a28
mengshanliao <r0918103121@gmail.com>
2024-08-22 00:54:30 +0800

    feat:update

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -78,24 +78,6 @@ router.post("/login", async (req, res) => {
     } else {
       res.status(401).send("密碼錯誤.");
     }
-    // User.findOne({ email: req.body.email }, function (err, user) {
-    //   if (err) {
-    //     res.status(400).send(err);
-    //   }
-    //   if (!user) {
-    //     res.status(401).send("User not found.");
-    //   } else {
-    //     user.comparePassword(req.body.password, function (err, isMatch) {
-    //       if (err) return res.status(400).send(err);
-    //       if (isMatch) {
-    //         const tokenObject = { _id: user._id, email: user.email };
-    //         const token = jwt.sign(tokenObject, process.env.PASSPORT_SECRET);
-    //         res.send({ success: true, token: "JWT " + token, user });
-    //       } else {
-    //         res.status(401).send("Wrong password.");
-    //       }
-    //     });
-    //   }
   });
 });
 
